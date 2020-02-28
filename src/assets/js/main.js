7a796cb1496c44c7e23b5d92fcb29ab882cecb77
Mykola Melnyk <melnyk.cto.numo@gmail.com>
2020-02-28 17:25:16 +0200

    add video

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -59,4 +59,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    // Smooth scroll anchor
+    $(".menu").on("click", "a", function (event) {
+        event.preventDefault();
+        const id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
 });
