80328f776584a2b5c2cb43eddc01d97c77137a9e
Mykola <melnyk.cto.numo@gmail.com>
2020-06-24 02:14:22 +0300

    fix: corrects 2

--- src/ajax/mail.php
+++ src/ajax/mail.php
@@ -11,16 +11,16 @@
     $mobileNumber = $_POST['mobileNumber'];
 
     $subject = "=?utf-8?B?" . base64_encode("Step Fill Result") . "?=";
-    $message = "\n\nWhat’s the Real Value of My Home?: " . $emailAddress . "\n\n
-                \n\nWhat kind of property do you own?: " . $kindOfProperty . "\n\n
-                \n\nHow long have you lived in your property?: " . $lengthOfStay . "\n\n
-                \n\nHow would you rate the condition of your home?: " . $homeConditionRate . "\n\n
-                \n\nHow would you rate your neighborhood?: " . $neighborhoodRate . "\n\n
-                \n\nStreet Address: " . $streetAddress . "\n\n
-                \n\nUnit number: " . $unitNumber . "\n\n
-                \n\nWhat’s the Real Value of My Home?: " . $sell . "\n\n
-                \n\nName: " . $name . "\n\n
-                \n\nMobile Number: " . $mobileNumber . "\n\n";
+    $message = "\r\nWhat’s the Real Value of My Home?: " . $emailAddress . "\r\n
+                \r\nWhat kind of property do you own?: " . $kindOfProperty . "\r\n
+                \r\nHow long have you lived in your property?: " . $lengthOfStay . "\r\n
+                \r\nHow would you rate the condition of your home?: " . $homeConditionRate . "\r\n
+                \r\nHow would you rate your neighborhood?: " . $neighborhoodRate . "\r\n
+                \r\nStreet Address: " . $streetAddress . "\r\n
+                \r\nUnit number: " . $unitNumber . "\r\n
+                \r\nWhat’s the Real Value of My Home?: " . $sell . "\r\n
+                \r\nName: " . $name . "\r\n
+                \r\nMobile Number: " . $mobileNumber . "\r\n";
     $headers = "From: $emailAddress\r\nReply-to: $emailAddress\r\nContent-type: text/html; charset=utf-8\r\n";
 
     $success = mail("melnyk.cto.numo@gmail.com", $subject, $message, $headers);
